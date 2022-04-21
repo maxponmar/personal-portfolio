8b74c26ff8a056d8cd474fa88a9610060fd3845d
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-21 04:46:20 -0700

    Implement home section

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,14 +1,15 @@
-import profileImage from "./assets/profile.webp";
+import profileImage from "../assets/profile.webp";
 
 const Home: React.FC<{}> = () => {
     return (
-        <div className="text-center">
-            <img className="rounded-full" src={profileImage} />
-            <h1 className="mt-10 text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-black to-blue-900 hover:-translate-y-1 hover:scale-105 duration-75 ease-in cursor-pointer">Maximiliano Ponce Marquez</h1>
+        <div className="flex flex-col items-center justify-center text-center h-screen lg:flex-row">
+            <img className="rounded-full w-1/2 sm:w-1/3 md:w-1/5 lg:w-60" src={profileImage} />
+            <div>
+                <h1 className="mt-1 px-10 text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-black to-blue-900 hover:-translate-y-1 hover:scale-105 duration-75 ease-in cursor-pointer sm:text-4xl sm:px-3">Maximiliano Ponce Marquez</h1>
+                <p>Mechatronics Engineer &amp; Software Developer</p>
+            </div>
         </div>
     )
 }
 
 export default Home;
-
-
