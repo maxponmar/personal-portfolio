fa8b3568bd12c82e014676e9480f44766a0e4e6b
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-21 04:11:45 -0700

    Move home conent to home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import profileImage from "./assets/profile.webp";
+import Home from "./sections/Home";
 
 const App: React.FC<{}> = () => {
   return (
-    <div className="text-center">
-      <img className="rounded-full" src={profileImage} />
-      <h1 className="mt-10 text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-black to-blue-900 hover:-translate-y-1 hover:scale-105 duration-75 ease-in cursor-pointer">Maximiliano Ponce Marquez</h1>
+    <div>
+      <Home />
     </div>
   )
 }
