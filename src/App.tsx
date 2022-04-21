adc5bc18e8f2be7ef613bd172d20ecb9fde72b20
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-20 19:56:10 -0700

    Add home page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 
 function App() {
   return (
-    <div>
-      Personal Portfolio
+    <div className="text-center">
+      <h1 className="mt-10 text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-black to-blue-900 hover:-translate-y-1 hover:scale-105 duration-75 ease-in cursor-pointer">Maximiliano Ponce Marquez</h1>
     </div>
   )
 }
 
-export default App
+export default App;
