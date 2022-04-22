f999a0b0a3c7de443ccb29047960a43c254ae804
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-21 19:13:42 -0700

    Create header component.

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import SectionLink from "./Link";
+
+const Header: React.FC<{}> = () => {
+    return (
+        <header className="w-screen text-center font-bold text-white bg-slate-900 fixed top-0">
+            <SectionLink title="Home" />
+            <SectionLink title="About" />
+            <SectionLink title="Skills" />
+            <SectionLink title="Projects" />
+            <SectionLink title="Contact" />
+        </header>
+    )
+}
+export default Header;
