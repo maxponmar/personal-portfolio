efd85df9c804f4a3641f2e25e8e03d6fd30204c1
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-21 19:14:57 -0700

    Update home section.
    - Add space image background.
    - Set dark background.
    - Implement color blur card.

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -2,13 +2,22 @@ import profileImage from "../assets/profile.webp";
 
 const Home: React.FC<{}> = () => {
     return (
-        <div className="flex flex-col items-center justify-center text-center h-screen lg:flex-row">
-            <img className="rounded-full w-1/2 sm:w-1/3 md:w-1/5 lg:w-60" src={profileImage} />
-            <div>
-                <h1 className="mt-1 px-10 text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-black to-blue-900 hover:-translate-y-1 hover:scale-105 duration-75 ease-in cursor-pointer sm:text-4xl sm:px-3">Maximiliano Ponce Marquez</h1>
-                <p>Mechatronics Engineer &amp; Software Developer</p>
+        <div className="bg-[url('./assets/space.webp')] bg-cover bg-fixed mt-[32px]">
+            <div className="bg-black bg-opacity-80 flex flex-col items-center justify-center text-center h-[calc(100vh-32px)] lg:flex-row">
+                <img className="rounded-full w-1/2 sm:w-72 lg:w-60 mr-2" src={profileImage} />
+                <div className="text-white">
+                    <h1 className="relative mt-1 px-10 text-3xl font-extrabold  group-hover:scale-[1.005] duration-100 ease-in cursor-pointer sm:text-4xl sm:px-3">Maximiliano Ponce Marquez</h1>
+                    <div className="relative group mt-4 inline-block">
+                        <div className="absolute -inset-0 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
+                        <p className="relative bg-neutral-900 rounded-lg px-5 ">
+                            &#128104;&#8205;&#127981; Mechatronics Engineer &amp; Software Developer &#128421;&#65039;
+                        </p>
+                    </div>
+                </div>
+                <a className="text-white absolute bottom-5 right-10" href="https://www.freepik.com/photos/night">Night photo created by wirestock - www.freepik.com</a>
             </div>
         </div>
+
     )
 }
 
