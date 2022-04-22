ac587f8e474d9d4c58b22dec985828c6f5f7ff83
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-22 04:41:52 -0700

    Use button for contact link.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,17 @@
+import Button from "../Button";
 import SectionLink from "./Link";
 
 const Header: React.FC<{}> = () => {
     return (
-        <header className="w-screen text-center font-bold text-white bg-slate-900 fixed top-0 z-50">
-            <SectionLink title="Home" />
-            <SectionLink title="About" />
-            <SectionLink title="Skills" />
-            <SectionLink title="Projects" />
-            <SectionLink title="Contact" />
+        <header className="w-screen h-10 text-center font-bold text-white text-sm bg-slate-900 fixed top-0 z-50 flex">
+            <div className="m-auto flex">
+                <SectionLink title="Home" />
+                <SectionLink title="About" />
+                <SectionLink title="Skills" />
+                <SectionLink title="Projects" />
+            </div>
+            {/* absolute right-10 top-1 */}
+            <Button title="Contact" customClass="h-8 m-auto right-10 top-1 sm:absolute" />
         </header>
     )
 }
