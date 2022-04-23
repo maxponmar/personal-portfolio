8826a17cca3aa26bd18c7ea72894fa4040389e67
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-23 12:05:33 -0700

    Create skills component.

---
+++ src/sections/skills/Skills.tsx
@@ -0,0 +1,10 @@
+import Section from "../../components/Section";
+
+const Skills: React.FC<{}> = () => {
+    return (
+        <Section title="Skils">
+            XD
+        </Section>
+    )
+}
+export default Skills;
