6ccb01853f8bfc05fc4b5c3fa39521bb348f2708
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-23 12:30:50 -0700

    Add react scroll motion dependency.

--- src/sections/home/Home.tsx
+++ src/sections/home/Home.tsx
@@ -4,7 +4,7 @@ import Typewriter from "typewriter-effect";
 const Home: React.FC<{}> = () => {
     return (
         <div className="bg-[url('./assets/space.webp')] bg-cover bg-fixed mt-[32px]">
-            <div className="bg-black bg-opacity-60 flex flex-col items-center justify-center text-center h-[calc(100vh-32px)] lg:flex-row">
+            <div className="bg-black bg-opacity-40 flex flex-col items-center justify-center text-center h-[calc(100vh-32px)] lg:flex-row">
                 <img className="rounded-full w-1/2 sm:w-72 lg:w-60 mr-2" src={profileImage} />
                 <div className="text-white">
                     <h1 className="relative mt-1 px-10 text-3xl font-extrabold  group-hover:scale-[1.005] duration-100 ease-in cursor-default sm:text-4xl sm:px-3 hover:scale-[1.01]">
@@ -14,10 +14,10 @@ const Home: React.FC<{}> = () => {
                             }}
                         />
                     </h1>
-                    <div className="relative group mt-4 inline-block">
+                    <div className="relative group mt-4 mx-3 inline-block">
                         <div className="absolute -inset-0 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
                         <p className="relative bg-neutral-900 rounded-lg px-5 cursor-default text-sm md:text-base">
-                            &#128104;&#8205;&#127981; Mechatronics Engineer &amp; Software Developer &#128421;&#65039;
+                            &#128104;&#8205;&#127981; Mechatronics Engineer &amp; <span className="block sm:inline-block">Software Developer &#128421;&#65039;</span>
                         </p>
                     </div>
                 </div>
