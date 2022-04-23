f960dc7df078659d40d0008a275ece9345c7bc52
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-23 12:04:07 -0700

    Update section component styles.

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 
 const Section: React.FC<{ title: string, children: ReactNode }> = ({ children, title }) => {
     return (
-        <div className="h-[calc(100vh-32px)] py-8 sm:py-12 lg:py-24 text-center ">
-            <h2 className="font-bold text-2xl md:text-3xl">{title}</h2>
+        <div className="h-[calc(100vh-32px)] py-8 sm:py-12 lg:py-24 text-center flex flex-col justify-center items-center">
+            <h2 className="font-bold text-2xl md:text-3xl mb-3 md:mb-16">{title}</h2>
             {children}
         </div>
     )
