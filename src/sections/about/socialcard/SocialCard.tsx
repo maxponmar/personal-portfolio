8f0aa1b30dd044bec804fd3ee386e30bb67a32ec
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-23 12:05:07 -0700

    Implement social card component.

---
+++ src/sections/about/socialcard/SocialCard.tsx
@@ -0,0 +1,13 @@
+import SocialCardModel from "./SocialCard.model";
+
+const SocialCard: React.FC<{ socialCard: SocialCardModel }> = ({ socialCard }) => {
+    return (
+        <div className="w-5/6 sm:w-3/4 md:w-1/3 lg:w-1/3 px-5 py-1 my-2 lg:mx-2 text-sm bg-purple-600 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20">
+            <h3>{socialCard.title}</h3>
+            <a href={socialCard.href} target="_blank">
+                <img className="h-7 lg:h-10 m-auto" src={socialCard.icon} />
+            </a>
+        </div>
+    )
+}
+export default SocialCard;
