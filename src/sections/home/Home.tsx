9864535f6268d06dd3a7b79ae22e86fd30075ead
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-23 12:24:27 -0700

    Add typing animation to home section.

--- src/sections/home/Home.tsx
+++ src/sections/home/Home.tsx
@@ -1,4 +1,5 @@
 import profileImage from "../../assets/profile.webp";
+import Typewriter from "typewriter-effect";
 
 const Home: React.FC<{}> = () => {
     return (
@@ -6,7 +7,13 @@ const Home: React.FC<{}> = () => {
             <div className="bg-black bg-opacity-60 flex flex-col items-center justify-center text-center h-[calc(100vh-32px)] lg:flex-row">
                 <img className="rounded-full w-1/2 sm:w-72 lg:w-60 mr-2" src={profileImage} />
                 <div className="text-white">
-                    <h1 className="relative mt-1 px-10 text-3xl font-extrabold  group-hover:scale-[1.005] duration-100 ease-in cursor-default sm:text-4xl sm:px-3 hover:scale-[1.01]">Maximiliano Ponce Marquez</h1>
+                    <h1 className="relative mt-1 px-10 text-3xl font-extrabold  group-hover:scale-[1.005] duration-100 ease-in cursor-default sm:text-4xl sm:px-3 hover:scale-[1.01]">
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter.typeString("Maximiliano Ponce Marquez").start();
+                            }}
+                        />
+                    </h1>
                     <div className="relative group mt-4 inline-block">
                         <div className="absolute -inset-0 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
                         <p className="relative bg-neutral-900 rounded-lg px-5 cursor-default text-sm md:text-base">
