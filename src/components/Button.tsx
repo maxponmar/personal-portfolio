fd62b59bae0166c3b55749b759b01c03d5728fef
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-24 05:18:09 -0700

    Update home and button to scroll to section.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,10 @@
-const Button: React.FC<{ title: string, customClass?: string }> = ({ title, customClass }) => {
+const Button: React.FC<{ title: string, customClass?: string, onClick: Function }> = ({ title, customClass, onClick }) => {
     const baseClass = "bg-purple-700 hover:bg-purple-900 text-white font-bold py-1 px-2 rounded ";
     const appliedClass = customClass ? baseClass + customClass : baseClass;
     return (
-        <button className={appliedClass}>{title}</button>
+        <button className={appliedClass} onClick={() => {
+            onClick();
+        }}>{title}</button>
     )
 }
 export default Button;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,20 @@
 import Button from "../Button";
-import SectionLink from "./Link";
+import useScroll, { SectionId } from "./ScrollHook";
+import SectionLink from "./SectionLink";
 
 const Header: React.FC<{}> = () => {
+    const scrollToSection = useScroll();
     return (
         <header className="w-screen h-10 text-center font-bold text-white text-sm bg-slate-900 fixed top-0 z-50 flex">
             <div className="m-auto flex">
-                <SectionLink title="Home" />
-                <SectionLink title="About" />
-                <SectionLink title="Skills" />
-                <SectionLink title="Projects" />
+                <SectionLink title="Home" sectionId={SectionId.Home} />
+                <SectionLink title="About" sectionId={SectionId.About} />
+                <SectionLink title="Skills" sectionId={SectionId.Skills} />
+                <SectionLink title="Projects" sectionId={SectionId.Projects} />
             </div>
-            {/* absolute right-10 top-1 */}
-            <Button title="Contact" customClass="h-8 m-auto right-10 top-1 sm:absolute" />
+            <Button title="Contact" customClass="h-8 m-auto right-10 top-1 sm:absolute" onClick={() => {
+                scrollToSection(SectionId.Contact);
+            }} />
         </header>
     )
 }
