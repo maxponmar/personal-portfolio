67e7f81b56d40fcf9d87860774a484c3d5a0e5aa
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-25 04:44:03 -0700

    Add skills to skills section.

--- src/sections/skills/Skills.tsx
+++ src/sections/skills/Skills.tsx
@@ -1,9 +1,19 @@
 import Section from "../../components/Section";
+import htmlIcon from "../../assets/html.svg";
+import cssIcon from "../../assets/css.svg";
+import jsIcon from "../../assets/javascript.svg";
+import reactIcon from "../../assets/react.svg";
+import SkillIcon from "./SkillIcon";
 
 const Skills: React.FC<{}> = () => {
     return (
         <Section title="Skils">
-            XD
+            <div className="flex gap-4">
+                <SkillIcon title="HTML" icon={htmlIcon} />
+                <SkillIcon title="CSS" icon={cssIcon} />
+                <SkillIcon title="JavaScript" icon={jsIcon} />
+                <SkillIcon title="ReactJS" icon={reactIcon} />
+            </div>
         </Section>
     )
 }
