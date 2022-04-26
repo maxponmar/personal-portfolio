3f0a1f10feec3f172b7a9bd5b296f8a8bf2b2c79
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-26 05:37:13 -0700

    Update about section sizes.

--- src/sections/about/About.tsx
+++ src/sections/about/About.tsx
@@ -7,8 +7,8 @@ const About: React.FC<{}> = () => {
     return (
         <Section title="About">
             <div className="flex flex-col justify-center items-center">
-                <img className="rounded-md w-7/12 max-w-xl sm:w-5/12 md:w-4/12 lg:w-1/3 mt-5 lg:mt-0" src={aboutImage} />
-                <div className="m-2 lg:text-2xl lg:my-10 sm:w-2/3">
+                <img className="rounded-md w-full max-w-lg md:w-10/12 lg:w-2/3 mt-5 lg:mt-0" src={aboutImage} />
+                <div className="m-2 text-xl lg:text-2xl lg:my-10 sm:w-2/3">
                     <span className="text-cyan-900 font-bold">Mechatronic Engineer &#128104;&#8205;&#127891;
                     </span> from <a className="underline underline-offset-2" href="http://www.itnogales.edu.mx/" target="_blank">Tecnológico Nacional de México - Campus Nogales</a> and <span className="text-cyan-900 font-bold">Software Developer &#10084;&#65039;
                     </span> by heart.
