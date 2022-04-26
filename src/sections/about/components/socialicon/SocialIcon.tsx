fc17fdc7d96ebb47fc32e8f77632ae29c1728a35
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-04-26 05:37:56 -0700

    Update social icon size.

--- src/sections/about/components/socialicon/SocialIcon.tsx
+++ src/sections/about/components/socialicon/SocialIcon.tsx
@@ -3,7 +3,7 @@ import SocialIconModel from "./SocialIcon.model";
 const SocialIcon: React.FC<{ socialIcon: SocialIconModel }> = ({ socialIcon }) => {
     return (
         <a className="mx-3" href={socialIcon.href} target="_blank">
-            <img className="h-7 lg:h-10" src={socialIcon.icon} />
+            <img className="w-10" src={socialIcon.icon} />
         </a>
     )
 }
