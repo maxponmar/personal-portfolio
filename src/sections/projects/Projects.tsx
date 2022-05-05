9ce00053bdbfad0f064ff2d5370b7d658db9aab0
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-05 05:35:40 -0700

    Implement responsive layout.

--- src/sections/projects/Projects.tsx
+++ src/sections/projects/Projects.tsx
@@ -4,7 +4,14 @@ import mecatronicafromscratch from "../../assets/projects/mecatronicafromscratch
 
 const Projects = () => {
   return <Section title="Projects">
-    <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
+    <div className="flex flex-wrap items-center justify-center gap-4">
+      <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
+      <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
+      <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
+      <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
+      <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
+      <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
+    </div>
   </Section>
 }
 
