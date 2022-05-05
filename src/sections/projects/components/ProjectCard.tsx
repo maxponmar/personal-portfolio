70ec32d1e7d448e5af2f1c04cd939f137111128c
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-05 05:37:17 -0700

    Add hover animation.

--- src/sections/projects/components/ProjectCard.tsx
+++ src/sections/projects/components/ProjectCard.tsx
@@ -8,7 +8,7 @@ interface ProjectCardModel {
 }
 const ProjectCard = ({ title, description, image, alt, buttonText, link }: ProjectCardModel) => {
   return (
-    <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm">
+    <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm hover:scale-105 duration-75">
       <img className="rounded-t-lg" src={image} alt={alt} />
       <div className="p-5">
         <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">{title}</h5>
