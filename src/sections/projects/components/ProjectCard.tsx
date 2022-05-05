423303f98fc80447866a6010d2d35355d26d99b4
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-05 05:33:12 -0700

    Set target blank for a tag.

--- src/sections/projects/components/ProjectCard.tsx
+++ src/sections/projects/components/ProjectCard.tsx
@@ -14,7 +14,7 @@ const ProjectCard = ({ title, description, image, alt, buttonText, link }: Proje
         <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">{title}</h5>
         <p className="font-normal text-gray-700 mb-3">{description}</p>
         {buttonText && link &&
-          <a className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center" href={link}>
+          <a className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center" href={link} target="_blank">
             {buttonText}
           </a>
         }
