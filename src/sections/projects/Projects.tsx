a61a5c9aebbc2d36592ab4dc9e4bb97c2a7c30eb
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-05 05:39:34 -0700

    Add todo comment.
    Fix responsive layout for small screens (height).

--- src/sections/projects/Projects.tsx
+++ src/sections/projects/Projects.tsx
@@ -4,6 +4,7 @@ import mecatronicafromscratch from "../../assets/projects/mecatronicafromscratch
 
 const Projects = () => {
   return <Section title="Projects">
+    {/* TODO: Fix responsive layout for small screens (height) */}
     <div className="flex flex-wrap items-center justify-center gap-4">
       <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
       <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
