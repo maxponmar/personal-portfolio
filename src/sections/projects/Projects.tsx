018029e602b9db751e612c6d7eea22352ab54059
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-05 05:09:05 -0700

    Add one project card in projects section.

--- src/sections/projects/Projects.tsx
+++ src/sections/projects/Projects.tsx
@@ -1,7 +1,10 @@
 import Section from "../../components/Section"
+import ProjectCard from "./components/ProjectCard";
 
 const Projects = () => {
-  return <Section title="Projects"><h2>Hello world</h2></Section>
+  return <Section title="Projects">
+    <ProjectCard />
+  </Section>
 }
 
 export default Projects;
