d31b9ab9c3dcef7878f73212215217978b66c9c7
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-05 05:33:23 -0700

    Create example ProjectCard component.

--- src/sections/projects/Projects.tsx
+++ src/sections/projects/Projects.tsx
@@ -1,9 +1,10 @@
 import Section from "../../components/Section"
 import ProjectCard from "./components/ProjectCard";
+import mecatronicafromscratch from "../../assets/projects/mecatronicafromscratch.png";
 
 const Projects = () => {
   return <Section title="Projects">
-    <ProjectCard />
+    <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
   </Section>
 }
 
