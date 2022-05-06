09ebebcab164048326f1f36ddb97b1a25ffa88c0
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-06 04:21:19 -0700

    Render projects from list.

--- src/sections/projects/Projects.tsx
+++ src/sections/projects/Projects.tsx
@@ -1,17 +1,15 @@
 import Section from "../../components/Section"
 import ProjectCard from "./components/ProjectCard";
-import mecatronicafromscratch from "../../assets/projects/mecatronicafromscratch.png";
+import ProjectsList from "./data/projectsData";
+
 
 const Projects = () => {
   return <Section title="Projects">
     {/* TODO: Fix responsive layout for small screens (height) */}
     <div className="flex flex-wrap items-center justify-center gap-4">
-      <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
-      <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
-      <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
-      <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
-      <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
-      <ProjectCard title="Mecatronica from Scratch" description="Website blog aimed for mechatronics engineering students." image={mecatronicafromscratch} alt="Mechatronica from Scratch" buttonText="Live Demo" link="https://maxponmar.github.io/MecatronicaFromScratch/#/home" />
+      {ProjectsList.map((project, index) =>
+        <ProjectCard key={index} project={project} />
+      )}
     </div>
   </Section>
 }
