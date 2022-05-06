e15bc583b80857dd59840cdcc3f7bb9a79a7ae9a
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-06 04:21:29 -0700

    Update project card component.

--- src/sections/projects/components/ProjectCard.tsx
+++ src/sections/projects/components/ProjectCard.tsx
@@ -1,21 +1,15 @@
-interface ProjectCardModel {
-  title: string;
-  description: string;
-  image: string;
-  alt: string;
-  buttonText?: string;
-  link?: string;
-}
-const ProjectCard = ({ title, description, image, alt, buttonText, link }: ProjectCardModel) => {
+import { Project } from "../data/projectsData";
+
+const ProjectCard = ({ project }: { project: Project }) => {
   return (
-    <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm hover:scale-105 duration-75">
-      <img className="rounded-t-lg" src={image} alt={alt} />
+    <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm h-96 hover:scale-105 duration-75">
+      <img className="rounded-t-lg w-full max-h-48 mx-auto" src={project.image} alt={project.alt} />
       <div className="p-5">
-        <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">{title}</h5>
-        <p className="font-normal text-gray-700 mb-3">{description}</p>
-        {buttonText && link &&
-          <a className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center" href={link} target="_blank">
-            {buttonText}
+        <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">{project.title}</h5>
+        <p className="font-normal text-gray-700 mb-3">{project.description}</p>
+        {project.buttonText && project.link &&
+          <a className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center" href={project.link} target="_blank">
+            {project.buttonText}
           </a>
         }
       </div>
