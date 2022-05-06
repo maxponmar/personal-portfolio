b8c49165c871f859264a6f1da618807a8687ab8f
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-05-06 04:29:06 -0700

    Update project modal.
    Remove the alt property as it always is the project title.

--- src/sections/projects/components/ProjectCard.tsx
+++ src/sections/projects/components/ProjectCard.tsx
@@ -2,8 +2,8 @@ import { Project } from "../data/projectsData";
 
 const ProjectCard = ({ project }: { project: Project }) => {
   return (
-    <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm h-96 hover:scale-105 duration-75">
-      <img className="rounded-t-lg w-full max-h-48 mx-auto" src={project.image} alt={project.alt} />
+    <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm hover:scale-105 duration-75">
+      <img className="rounded-t-lg w-full max-h-48 mx-auto" src={project.image} alt={project.title} />
       <div className="p-5">
         <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">{project.title}</h5>
         <p className="font-normal text-gray-700 mb-3">{project.description}</p>
